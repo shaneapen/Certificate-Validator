09931c8540ac7ccc61ee1837d3b67cdcb9304156
Shan Eapen Koshy <shaneapen@gmail.com>
2019-07-16 21:37:16 +0530

    fixed #2

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ $url = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 // Random id EB5C starts at 7th index from the slash part of URL
 $full_id = substr($url, strrpos($url, '/') + 1);
 
+if($_POST['certificateID']){
+ $full_id = $_POST['certificateID'];
+};
+
 $event = substr($full_id,0,4);
 $year = substr($full_id,4,2);
 $userID = substr($full_id,6,4);
@@ -22,9 +26,9 @@ if(file_exists($certificate_file) && file_exists($json_file)){
       if(array_key_exists($userID, $json))
             include $certificate_file;
       else
-            echo "Not found";
+            include '404.html';
             
 }else
-    echo "Not found!";
+      include '404.html';
 
 ?>
