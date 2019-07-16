c247c9f9f82ca88214835efdbd6d3903aaf4c35d
Shan Eapen Koshy <shaneapen@gmail.com>
2019-07-16 22:33:42 +0530

    added favicon

--- data/ISQP/19/certificate.php
+++ data/ISQP/19/certificate.php
@@ -8,6 +8,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="https://cecieee.org/wp-content/uploads/2018/12/cropped-favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" href="https://cecieee.org/wp-content/uploads/2018/12/cropped-favicon-192x192.png" sizes="192x192" />
 </head>
 <body>
 <style>
